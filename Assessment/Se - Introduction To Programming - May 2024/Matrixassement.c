d105c5285d2c8d7659992b94b56eab62b959d365
dhvanit2804 <dhvanitparate123@gmail.com>
2025-08-26 11:24:22 +0530

    26-08-2025

---
+++ Assessment/Se - Introduction To Programming - May 2024/Matrixassement.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+// Function to take matrix input
+void inputMatrix(int mat[10][10], int row, int col) {
+    int i, j;
+    printf("\nEnter elements of matrix (%d x %d):\n", row, col);
+    for(i = 0; i < row; i++) {
+        for(j = 0; j < col; j++) {
+            printf("Enter element [%d][%d]: ", i+1, j+1);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+// Function to display matrix
+void displayMatrix(int mat[10][10], int row, int col) {
+    int i, j;
+    printf("\nMatrix (%d x %d):\n", row, col);
+    for(i = 0; i < row; i++) {
+        for(j = 0; j < col; j++) {
+            printf("%5d", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Function to multiply two matrices
+void multiplyMatrix(int a[10][10], int b[10][10], int result[10][10], int r1, int c1, int c2) {
+    int i, j, k;
+
+    // Initialize result matrix with 0
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c2; j++) {
+            result[i][j] = 0;
+        }
+    }
+
+    // Perform multiplication
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c2; j++) {
+            for(k = 0; k < c1; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int a[10][10], b[10][10], result[10][10];
+    int r1, c1, r2, c2;
+
+    printf("----- Matrix Multiplication Program -----\n");
+
+    // Input rows and columns for first matrix
+    printf("\nEnter rows and columns for first matrix: ");
+    scanf("%d%d", &r1, &c1);
+
+    // Input rows and columns for second matrix
+    printf("Enter rows and columns for second matrix: ");
+    scanf("%d%d", &r2, &c2);
+
+    // Check multiplication condition
+    if(c1 != r2) {
+        printf("\nMatrix multiplication is not possible!");
+        printf("\n(Number of columns of first matrix must equal rows of second matrix)\n");
+        return 0;
+    }
+
+    // Take input for both matrices
+    inputMatrix(a, r1, c1);
+    inputMatrix(b, r2, c2);
+
+    // Display entered matrices
+    displayMatrix(a, r1, c1);
+    displayMatrix(b, r2, c2);
+
+    // Multiply matrices
+    multiplyMatrix(a, b, result, r1, c1, c2);
+
+    // Display result
+    printf("\nResultant Matrix after multiplication:\n");
+    displayMatrix(result, r1, c2);
+
+    return 0;
+}
+

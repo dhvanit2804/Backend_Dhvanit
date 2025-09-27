9f48f9ec2d160910affe5a65a926dcc863eb4c1b
dhvanit2804 <dhvanitparate123@gmail.com>
2025-09-27 14:36:12 +0530

    27-9-2025

---
+++ Practice/Python/If Elseif else/forloop.py
@@ -0,0 +1,14 @@
+'PYRAMID'
+print("**************PYRAMID PATTERN***************")
+for i in range(1,10):
+    print(" "*(9-i)," *" * i)
+
+'Right'
+print("**************RIGHT PATTERN***************")
+for i in range(1,10):
+    print("*" * i)
+
+'LEFT'
+print("**************LEFT PATTERN***************")
+for i in range(1, 10):
+    print(" " * (9-i),"*" * i)
---
+++ Practice/Python/If Elseif else/password.py
@@ -0,0 +1,8 @@
+password = "Dhvanit123"
+
+user_input = input("Enter a password: ")
+
+if user_input == password:
+    print("Access Granted")
+else:
+    print("Access Denied")

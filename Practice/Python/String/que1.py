ab08c8847ffd629c30838f2564c9c2ad37fd84d1
dhvanit2804 <dhvanitparate123@gmail.com>
2025-10-03 21:57:58 +0530

    3-10-2025

---
+++ Practice/Python/String/que1.py
@@ -0,0 +1,27 @@
+s = input("Enter a string: ")
+print(s)
+
+al = 0
+nm = 0
+sp = 0
+uc = 0
+lc = 0
+
+for i in s:
+    
+    if i.isalpha() :
+        al = al + 1
+    elif i.isnumeric() :
+        nm = nm + 1
+    elif i.isspace():
+        sp = sp + 1
+    if i.isupper():
+        uc = uc + 1
+    elif i.islower():
+        lc = lc + 1
+
+print(f"Total Alphabets : {al}")
+print(f"Total numeric : {nm}")
+print(f"Total spaces : {sp}")
+print(f"Total uppercharcter : {uc}")
+print(f"Total lowercharcter : {lc}")

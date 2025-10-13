2ea4ccc9348f492fe362ba6ae80a575773710fc4
dhvanit2804 <dhvanitparate123@gmail.com>
2025-10-13 15:49:27 +0530

    13-10-2025

--- Practice/Python/File Input Output/file.py
+++ Practice/Python/File Input Output/file.py
@@ -1,15 +1,15 @@
-file = open("tops.txt","w")
+file = open("tops.txt","w") # w for write
 file.write("Welcome to the file management learning in a python")
 file.close()
 print("File Written Succesfully")
 print("*****************************************************")
 
-file = open("tops.txt","r")
+file = open("tops.txt","r") # r for read
 print(file.read())
 file.close()
 print("*****************************************************")
 
-file = open("tops.txt","a")
+file = open("tops.txt","a") # a for append
 file.write("\nFile Management With Python Is Very Easy To Learn.")
 file.close()
 print("*****************************************************")
@@ -18,3 +18,10 @@ file = open("tops.txt","r")
 print(file.read())
 file.close()
 print("*****************************************************")
+
+file = open("tops1.txt","w+") # w+ for write and read
+file.write("This is w+ operation in python.")
+print("File Current Position : ",file.tell())
+file.seek(0)
+print("File Data :",file.read())
+file.close()

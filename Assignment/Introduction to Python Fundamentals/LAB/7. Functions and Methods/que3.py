1460b2ba27f9a7dddde7d8600a2f04526dd938d7
dhvanit2804 <dhvanitparate123@gmail.com>
2025-10-13 07:30:48 +0530

    13-10-2025

---
+++ Assignment/Introduction to Python Fundamentals/LAB/7. Functions and Methods/que3.py
@@ -0,0 +1,7 @@
+'Practical Example: 3) Write a Python program to print a string using triple quotes. '
+
+text = '''This is
+multi-line
+string'''
+
+print(text)
---
+++ Assignment/Introduction to Python Fundamentals/LAB/7. Functions and Methods/que4.py
@@ -0,0 +1,6 @@
+'''Practical Example: 4) Write a Python program to access the first character of a string using 
+index value. '''
+
+word = "Python"
+
+print(word[0])

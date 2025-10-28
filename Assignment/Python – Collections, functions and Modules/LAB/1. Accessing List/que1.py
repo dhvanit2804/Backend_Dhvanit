44e7b0033a4d9048523cdf269d56e1a9ea990491
dhvanit2804 <dhvanitparate123@gmail.com>
2025-10-28 11:05:09 +0530

    28-10-2025

---
+++ Assignment/Python – Collections, functions and Modules/LAB/1. Accessing List/que1.py
@@ -0,0 +1,10 @@
+'''Write a Python program to create a list with elements of multiple data types (integers, 
+strings, floats, etc.). '''
+
+list = ["Dhvanit", "Rohan", 28, 74.77, False, "Sahil"]
+
+print(list)
+print(type(list[1]))
+print(type(list[2]))
+print(type(list[3]))
+print(type(list[4]))
---
+++ Assignment/Python – Collections, functions and Modules/LAB/1. Accessing List/que2.py
@@ -0,0 +1,9 @@
+'Write a Python program to access elements at different index positions.'
+
+list = ["Dhvanit", "Rohan", 28, 74.77, False, "Sahil"]
+
+print(list)
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])

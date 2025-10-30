3a550ab9872b1bc00ef32e4d12d3054c12a16aac
dhvanit2804 <dhvanitparate123@gmail.com>
2025-10-30 11:51:17 +0530

    30-10-2025

---
+++ Assessment/Python – Collections, functions and Modules/BookRental.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+rentals = []
+LATE_FEE_PER_DAY = 10
+
+def rent_book():
+    print("\n--- Book Rental Booking ---")
+    customer = input("Enter Customer Name: ")
+    book = input("Enter book title: ")
+    rental_date = input("Enter rental date (YYYY-MM-DD): ")
+    due_date = input("Enter expected return date (YYYY-MM-DD): ")
+
+    rental = {
+        "customer" : customer,
+        "book": book,
+        "rental_date": rental_date,
+        "due_date": due_date,
+        "returned": False
+    }
+
+    rentals.append(rental)
+    print(f"\n Book '{book}' rented successfully to {customer}.\n")
+
+rent_book()

ffb327608a81d1d745979021ff0c6daee42939d3
dhvanit2804 <dhvanitparate123@gmail.com>
2025-11-07 11:42:22 +0530

    7-11-2025

--- Practice/Django Project/myapp/views.py
+++ Practice/Django Project/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Contact
+from .models import Contact,User
 
 # Create your views here.
 def index(request):
@@ -21,7 +21,52 @@ def contact(request):
         return render(request,'contact.html',{'contacts':contacts})
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == "POST":
+        try:
+            User.objects.get(email=request.POST['email'])
+            msg = "Email Already Registered"
+            return render(request,'signup.html',{'msg':msg})
+        except:
+            if request.POST['password'] == request.POST['cpassword']:
+                User.objects.create(
+                    fname = request.POST['fname'],
+                    lname = request.POST['lname'],
+                    email = request.POST['email'],
+                    mobile = request.POST['mobile'],
+                    address = request.POST['address'],
+                    password = request.POST['password'],
+                )
+                msg ="User Sign Up Successfully!"
+                return render(request,'signup.html',{'msg':msg})
+            else:
+                msg = "Password & Confirm Password does not Matched!"
+                return render(request,'signup.html',{'msg':msg})
+    else:
+        return render(request,'signup.html')
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == "POST":
+        try:
+            user = User.objects.get(email=request.POST['email'])
+            if user.password==request.POST['password']:
+                request.session['email']=user.email
+                request.session['fname']=user.fname
+                return render(request,'index.html')
+            else:
+                msg = "Incorrect Password"
+                return render(request,'login.html',{'msg':msg})
+        except:
+                msg = "Email Not Registered"
+                return render(request,'login.html',{'msg':msg})
+    else:
+        return render(request,'login.html')
+    
+def logout(request):
+    try:
+        del request.session['email']
+        del request.session['fname']
+        msg = "Logged Out Successfully"
+        return render(request,'login.html',{'msg':msg})
+    except:
+        msg = "Logged Out Successfully"
+        return render(request,'login.html',{'msg':msg})

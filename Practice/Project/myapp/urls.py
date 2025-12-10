4fe92ce92037a10f875f733c0c7af32854639e23
dhvanit <dhvanitparate123@gmail.com>
2025-12-10 22:27:54 +0530

    10-12-2025

--- Practice/Project/myapp/urls.py
+++ Practice/Project/myapp/urls.py
@@ -18,4 +18,12 @@ urlpatterns = [
     path('seller-product-details/<int:pk>/',views.seller_product_details,name='seller-product-details'),
     path('seller-product-edit/<int:pk>/',views.seller_product_edit,name='seller-product-edit'),
     path('seller-product-delete/<int:pk>/',views.seller_product_delete,name='seller-product-delete'),
+    path('men/',views.men,name='men'),
+    path('women/',views.women,name='women'),
+    path('kids/',views.kids,name='kids'),
+    path('accessories/',views.accessories,name='accessories'),
+    path('product-details/<int:pk>/',views.product_details,name='product-details'),
+    path('add-to-wishlist/<int:pk>/',views.add_to_wishlist,name='add-to-wishlist'),
+    path('wishlist/',views.wishlist,name='wishlist'),
+    path('remove-from-wishlist/<int:pk>/',views.remove_from_wishlist,name='remove-from-wishlist'),
 ]

67e546c5e7b2ab5abced68156ed2a83b65721c65
dhvanit2804 <dhvanitparate123@gmail.com>
2026-04-01 17:17:19 +0530

    restapi

---
+++ djangojwt/myapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        username= request.data.get('username')
+        password= request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            user_serializer = UserSerializer(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': user_serializer.data
+            })
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=400)
+
+class DashboardView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+        user_serializer = UserSerializer(user)
+        return Response({'message': 'Welcome to the dashboard!', 'user': user_serializer.data}, status=200)
